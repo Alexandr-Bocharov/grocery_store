b8cc8750b8b59f26d64993b4e009b7be05f26435
tanjiro <counter23062001@mail.ru>
2025-02-20 20:49:44 +0300

    update products models(add class Meta and verbose_name)

--- products/models.py
+++ products/models.py
@@ -7,9 +7,23 @@ class Category(models.Model):
     slug_name = models.SlugField()
     image = models.ImageField()
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
 
 class Subcategory(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug_name = models.SlugField()
     image = models.ImageField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
